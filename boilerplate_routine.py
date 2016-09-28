e5e729be02ba0e959b5ea21ff6d2ea81f0b57369
Ben Lewis <ben.lewis@ga.gov.au>
2016-09-28 11:38:40 +1000

    starting restructure

--- boilerplate_routine.py
+++ boilerplate_routine.py
@@ -21,6 +21,10 @@ import numpy as np
 import datacube.model.utils
 import yaml
 
+import click
+import pickle
+import itertools
+
 info = {'lineage': { 'algorithm': { 'name': "WOFS decision-tree water extents",
                                     'version': 'unknown',
                                     'repo_url': 'https://github.com/benjimin/wetness' }}}
@@ -72,9 +76,153 @@ measurements:
 """ # end of product definition
 
 
+def unpickle_stream(pickle_file):
+    """Utility to stream unpickled objects from a file"""
+    s = pickle.Unpickler(pickle_file)
+    while True:
+        try:
+            yield s.load()
+        except EOFError:
+            raise StopIteration
+            
+def map_orderless(core,tasks,queue=50):
+    """Utility to stream tasks through compute resources"""
+    import distributed # slow import
+    ex = distributed.Client() # executor  
+    
+    tasks = (i for i in tasks) # ensure input is a generator
+      
+    # pre-fill queue
+    results = [ex.submit(core,*t) for t in itertools.islice(tasks, queue)]
+           
+    while results:
+        result = next(distributed.as_completed(results)) # block
+        results.remove(result)                  # pop completed
+
+        task = next(tasks, None)
+        if task is not None:
+            results.append(ex.submit(core,*task)) # queue another
+        
+        yield result.result() # unwrap future
+
+
+
+class datacube_application:
+    """Nonspecific application workflow."""
+    def generate_tasks(self, index):
+        """Prepare list of task argument tuples."""
+        raise NotImplemented
+    def perform_task(self, *args):
+        """Do computation without database interaction"""
+        raise NotImplemented
+    def __call__(self, algorithm):
+        """Annotator API for application
+        
+        >>> @datacube_application(**options)
+        >>> def myfunction(input_chunk):
+        >>>     return output_chunk
+        """
+        self.core = algorithm
+        self.main(datacube.Datacube().index)
+    def main(self, index):
+        """Compatibility command-line-interface"""
+        
+        @click.group(name=self.core.__name__)
+        def cli():
+            pass
+        
+        @cli.command(help="Pre-query tiles for one calendar year.")
+        @click.argument('year', type=click.INT)
+        @click.argument('taskfile', type=click.File('w'))
+        @click.option('--max', default=0, help="Limit number of tasks")
+        def prepare(year, taskfile, max):
+            t = str(year)+'-01-01', str(year+1)+'-01-01'
+            print "Querying", t[0], "to", t[1]
+            stream = pickle.Pickler(taskfile)
+            i = 0
+            for task in self.generate_tasks(index, time=t):
+                stream.dump(task)
+                i += 1
+                if i==max:
+                    break
+            print i, "tasks prepared"         
+           
+        @cli.command(help="Read pre-queried tiles and distribute computation.")
+        @click.option('--backlog', default=50, help="Maximum queue length")
+        @click.argument('taskfile', type=click.File('r'))
+        def orchestrate(backlog, taskfile):
+            tasks = unpickle_stream(taskfile)
+            done_tasks = map_orderless(self.perform_task, tasks, queue=backlog)
+            for i,ds in enumerate(done_tasks):
+                print i
+                index.datasets.add(ds, skip_sources=True) # index completed work
+            print "Done"
+            
+        cli()
+
+
+class monkeypatch_application(datacube_application)
+    def main(self, index, max_tasks=2):
+        """Simplified main interface, for debugging"""
+        valid_loadables = list(self.generate_tasks(index)) # find work to do
+        print len(valid_loadables), "tasks in total"
+        valid_loadables = valid_loadables[:max_tasks] # trim for debugging.     
+        for task in valid_loadables:
+            print ".",
+            ds = taskdoer(*task) # do work
+            index.datasets.add(ds) # index completed work
+        print "Done."
+
+
+
+class wofloven(datacube_application):
+    """Specialisations for Water Observation product"""
+    def __init__(self, time=None, extent=None):
+        pass
+    def generate_tasks(self, index, time, extent):
+        """ Prepare task-parameters (a series of nbar,ps,dsm[,filename] loadables) for dispatch to workers.
+
+        The concept is that workers shall not interact with the database index.
+        Instead, workers must be supplied file-paths as necessary for data I/O.
+
+        This function is the equivalent of an SQL join query,
+        and is required as a workaround for datacube API abstraction layering.        
+        """
+        gw = datacube.api.GridWorkflow(index, product='ls5_nbar_albers') # clone GridSpec from EO archive 
+
+        wofls_loadables = gw.list_tiles(product=product.name, time=time, **extent)
+
+        for platform in sensor.keys():
+            source_loadables = gw.list_tiles(product=platform+'_nbar_albers', time=time, **extent)
+            pq_loadables = gw.list_tiles(product=platform+'_pq_albers', time=time, **extent)
+            dsm_loadables = gw.list_tiles(product='dsm1sv10', **extent)                
+
+            assert len(set(t for (x,y,t) in dsm_loadables)) == 1 # assume mosaic won't require extra fusing
+            dsm_loadables = {(x,y):val for (x,y,t),val in dsm_loadables.items()} # make mosaic atemporal
+
+            # only valid where EO, PQ and DSM are *all* available (and WOFL isn't yet)
+            keys = set(source_loadables) & set(pq_loadables) .difference(set(wofls_loadables))
+            # should sort spatially, consider repartitioning workload to minimise DSM reads.
+            for x,y,t in keys:
+                if (x,y) in dsm_loadables: # filter complete
+                    fn = filename_template.format(sensor=sensor[platform],
+                                                  tile_index=(x,y),
+                                                  time=pandas.to_datetime(t).strftime('%Y%m%d%H%M%S%f'))
+                    s,p,d = map(gw.update_tile_lineage, # fully flesh-out the metadata
+                                [ source_loadables[(x,y,t)], pq_loadables[(x,y,t)], dsm_loadables[(x,y)] ])
+                    yield ((s,p,d), pathlib.Path(destination,fn))
+
+
+
+    def perform_task(self, *args):
+        raise NotImplemented    
+
+
+
+
 def wofloven(time, **extent):
     """Annotator for WOFL workflow""" 
-    def main(core_func):
+    def main(core_func):time=time, extent=extent):
         """Continental-scale WOFL-specific machinery"""
 
         # The WOFS storage format will be chosen to mirror the EO archive.
@@ -231,16 +379,7 @@ def wofloven(time, **extent):
 
     return main
 
-def simplistic_app(index, taskmaker, taskdoer):
-    """Simplistic app (for debugging) just creates and indexes some of the desired tiles."""
-    valid_loadables = list(taskmaker(index)) # find work to do
-    print len(valid_loadables)
-    valid_loadables = valid_loadables[:2] # trim for debugging.     
-    for task in valid_loadables:
-        print ".",
-        ds = taskdoer(*task) # do work
-        index.datasets.add(ds) # index completed work
-    print "Done."
+
 
 def another_app(index, taskmaker, taskdoer, name="application"):
     """ Compatibility interface """
