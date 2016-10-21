e2c46da0c096814ded5d7cb9fca21a06fa511c25
Ben Lewis <ben.lewis@ga.gov.au>
2016-10-21 14:06:09 +1100

    added weight constants

--- ancillary.py
+++ ancillary.py
@@ -1,18 +1,22 @@
 """
 
-There are 11 ancillary inputs (from five sources) used by the confidence layer.
+There are 13 ancillary inputs (from five sources) for the confidence layer,
+including 2 that are unused (zero-weighted).
 
-These can be synthesized into a single once-off mosaic, for combining with
-the water observation frequencies.
+The algorithm permits these 11 to be synthesized into a single once-off mosaic,
+for combining with the water observation frequencies:
 
-This script prepares that mosaic.
+Confidence = LogisticFunction( beta_0 * wofl_summary + ancillary_layer_mosaic )
+
+This script prepares that mosaic, in the native resolution of wofs (albers).
 
 
 
 
 WOfS is specified by the journal article:
 http://dx.doi.org/10.1016/j.rse.2015.11.003
-Gives their linear weights.
+Gives their linear weights in table 4 (reproduced below as constants).
+This code ignores (obviously) the two additional variables with zero weights.
 
 Slope in decimal degrees. 
 Derived from the same (1s SRTM) DSM as already used for the extents.
@@ -21,17 +25,50 @@ Multi-resolution Valley Bottom Flatness
 http://doi.org/10.4225/08/5701C885AB4FE
 CSIRO; CC-BY 4.0 Int; 1 arcsecond; 0.7GB; single integer band
 
-Geofabric (9 vector layers)
+Geofabric (7 vector layers)
 https://data.gov.au/dataset/australian-hydrological-geospatial-fabric-geofabric
-BOM; CC-BY 3.0 Aus; v2.1.1; ESRI .gdb; 
+BOM; CC-BY 3.0 AU; v2.1.1; ESRI .gdb; 
 
 MODIS Open Water Likelihood
+CSIRO? ....???
 
-
-ABS ASGS 2011 Urban Centre and Locality
-
+Urban Centre and Locality (ASGS vol.4 2011)
+http://abs.gov.au/AUSSTATS/abs@.nsf/Lookup/1270.0.55.004Main+Features1July%202011?OpenDocument
+ABS; CC BY 2.5 AU; shapefile + csv; 30MB
 
 
 
 """
+
+# Weights from WOfS table: 
+wofl_wet_freq = 0.1703
+MrVBF = 0.1671
+MODIS_OWL = 0.0336
+slope = -0.2522
+geofabric_foreshore = 4.2062
+geofabric_pondage = -5.4692
+geofabric_reservoir = 0.6574
+geofabric_flat = 0.7700
+geofabric_lake = 1.9992
+geofabric_swamp = 1.3231
+geofabric_watercourse = 1.9206
+urban_areas	 = -4.9358
+# (note WOfS excludes geofabric canal and rapid, by zero weighting)
+
+
+"""
+Development strategy:
+
+The total mosaic could be of order 100GB uncompressed.
+A single file with chunked compression might perform well for localised reads,
+but will likely exceed memory availability.
+
+Therefore, split into tiles for creation, and consider amalgamating later.
+
+Initial approach of operating for arbitrary region.
+
+To start with, just plot to confirm correct reading of all 11 datasets.
+
+"""
+
 
